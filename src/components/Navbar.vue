7fdbbcc9df940ecf9d4e92aab32b1ad1f050a2ca
bulutarda <arda.bulut@ozu.edu.tr>
2022-04-06 14:08:54 +0300

    navbar updated

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="relative container mx-auto p-6 bg-gray-500">
+   <div class="flex items-center justify-between">
+       <div class="pt-2">
+           <img src="https://cdn.dribbble.com/users/238864/screenshots/15930284/media/6cd2840046dac3e19127137388f66ebd.png?compress=1&resize=400x300" class="w-28"/>
+       </div>
+       <div class="hidden md:flex space-x-6">
+           <a href="#" class="hover:text-darkGrayishBlue">Pricing</a>
+           <a href="#" class="hover:text-darkGrayishBlue">Product</a>
+           <a href="#" class="hover:text-darkGrayishBlue">About Us</a>
+           <a href="#" class="hover:text-darkGrayishBlue">Careers</a>
+           <a href="#" class="hover:text-darkGrayishBlue">Community</a>
+       </div>
+       <a href="" class=" hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">Get Started</a>
+
+   </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
