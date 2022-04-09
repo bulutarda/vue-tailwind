170b026e2f51975592b71f5e420c846ec3c37929
bulutarda <arda.bulut@ozu.edu.tr>
2022-04-09 23:58:23 +0300

    features updated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import Features from './components/Features.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <Features />
 </template>
 
 
---
+++ src/components/Features.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row">
+      <div class="flex flex-col space-y-12 md:w-1/2">
+         <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
+             What is a Demo?
+         </h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
